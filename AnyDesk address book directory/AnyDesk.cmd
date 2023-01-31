33d36fecd2a20e097a773aff1d5a7f6ae93940d9
rfptlsm <oua@HD.local>
2023-01-31 17:14:01 +0300

    Add scripts

---
+++ AnyDesk address book directory/AnyDesk.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+if exist "C:\Program Files (x86)\AnyDesk\" (
+	set PathToAnyDesk="C:\Program Files (x86)\AnyDesk\"
+	set run=AnyDesk.exe
+) else if exist "C:\Program Files (x86)\AnyDeskMSI\" (
+	set PathToAnyDesk=C:\"Program Files (x86)"\AnyDeskMSI\
+	set run=AnyDeskMSI.exe
+) else ( 
+echo dir not fount.
+timeout 5
+EXIT 0 )
+
+if exist "address.csv" (
+powershell "Import-Csv .\address.csv -Encoding UTF8 | Sort-Object Name"
+) else ( echo "address.csv not found")
+
+set /p addr=
+
+%PathToAnyDesk%\%run% %addr% --plain
+
+taskkill.exe /im anydesk* /f /t
